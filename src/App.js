6e428cf5b96cec943fcc936180113064c2f42565
RameshSBanakar <sb.ramesh@beta-cae.com>
2024-05-17 12:55:44 +0530

    background color and more

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from './component/Home/Home';
 import Navbar from "./component/Navbar/Navbar";
 function App() {
   return (
-    <div>
+    <div className='App'>
       <Navbar />
       <Home/>
     </div>
