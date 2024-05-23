e0e05eff92bb5533a0fae28dc9d549b62c2fe21b
RameshSBanakar <sb.ramesh@beta-cae.com>
2024-05-23 18:24:25 +0530

    view and download th pdf

--- src/component/SPDRM/Spdrm.js
+++ src/component/SPDRM/Spdrm.js
@@ -1,13 +1,16 @@
 import React from "react";
-import "./Spdrm.css";
+
+import { Document, Page } from "react-pdf";
+import { pdfjs } from "react-pdf";
+import "@react-pdf-viewer/core/lib/styles/index.css";
 
 import spderm_logo from "../Assets/SPDRM/logo_cropped_spdrm.png";
 import pdf_logo from "../Assets/pdf_logo.webp";
 import link_logo from "../Assets/link_logo.jpg";
 import youtube_logo from "../Assets/youtube_logo.png";
-import ppt_logo from "../Assets/ppt_logo.png"
+import ppt_logo from "../Assets/ppt_logo.png";
 import brochers_logo from "../Assets/brochers.png";
-import video_logo from "../Assets/video.png"
+import video_logo from "../Assets/video1.jpg";
 
 import SPDRM_BROCHER from "../Assets/SPDRM/spdrm_brochure.pdf";
 import SPDRM_QA from "../Assets/SPDRM/spdrm_qa.pdf";
@@ -16,17 +19,24 @@ import SPDRM_SUPERIORITY from "../Assets/SPDRM/spdrm_superiority.pdf";
 import SPDRM_VEHICLE_COLLISION from "../Assets/SPDRM/vehicle_collision.pdf";
 import POWER_OF_PROCESS_MANAGMENT from "../Assets/SPDRM/Unlocking_the_power_of_process_management_with_SPDRM.pdf";
 import MULTI_SITE_COLLABRATION from "../Assets/SPDRM/multisite_collabration.pdf";
-import SPDRM_ADM_GUIDE from "../Assets/SPDRM/spdrm_administrators_guide.pdf"
-import SPDRM_USER_GUIDE from "../Assets/SPDRM/spdrm_users_guide.pdf"
-import SPDRM_INSTALLATION_GUIDE from "../Assets/SPDRM/spdrm_installation_guide.pdf"
-import EXE_DESIGN_PROCESS from "../Assets/SPDRM/Execution_and_Design_Processes.pdf"
-import INTRODUCTION_TO_SPDRM from "../Assets/SPDRM/20240300_introduction_to_SPDRM.pdf"
-import SPDRM_TUTORIAL from "../Assets/SPDRM/spdrm_tutorial.pdf"
-import HM_FROM_SPDRM_VIDEO from "../Assets/SPDRM/Edit in HM from SPDRM.avi"
-import TC_SPDRM_VIDEO from "../Assets/SPDRM/TC-SPDRM.avi"
+import SPDRM_ADM_GUIDE from "../Assets/SPDRM/spdrm_administrators_guide.pdf";
+import SPDRM_USER_GUIDE from "../Assets/SPDRM/spdrm_users_guide.pdf";
+import SPDRM_INSTALLATION_GUIDE from "../Assets/SPDRM/spdrm_installation_guide.pdf";
+import EXE_DESIGN_PROCESS from "../Assets/SPDRM/Execution_and_Design_Processes.pdf";
+import INTRODUCTION_TO_SPDRM from "../Assets/SPDRM/20240300_introduction_to_SPDRM.pdf";
+import SPDRM_TUTORIAL from "../Assets/SPDRM/spdrm_tutorial.pdf";
+import HM_FROM_SPDRM_VIDEO from "../Assets/SPDRM/Edit in HM from SPDRM.avi";
+import TC_SPDRM_VIDEO from "../Assets/SPDRM/TC-SPDRM.avi";
+
+
+import "./Spdrm.css";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 
 const Spdrm = () => {
+  const handleDownload = (pdf) => {
+    window.open(pdf, "_blank");
+  };
   return (
     <div className="common">
       <div className="imagediv">
@@ -34,9 +44,12 @@ const Spdrm = () => {
       </div>
       <div className="datadiv">
         <div className="pdfFilesdiv">
-          
           <div className="innerdivs" id="pdfFilesMarginLeft">
-            <a href={INTRODUCTION_TO_SPDRM} download="introduction_to_SPDRM">
+            <a
+              // href={INTRODUCTION_TO_SPDRM}
+              onClick={() => handleDownload(INTRODUCTION_TO_SPDRM)}
+              download="introduction_to_SPDRM"
+            >
               <span className="colorAndFont">
                 Introduction to SPDRM
                 <img src={ppt_logo} className="doc-logo" />
@@ -45,7 +58,11 @@ const Spdrm = () => {
           </div>
 
           <div className="innerdivs" id="pdfFilesMarginLeft">
-            <a href={SPDRM_TUTORIAL} download="spdrm_tutorial">
+            <a
+              // href={SPDRM_TUTORIAL}
+              onClick={() => handleDownload(SPDRM_TUTORIAL)}
+              download="spdrm_tutorial"
+            >
               <span className="colorAndFont">
                 SPDRM tutorial
                 <img src={pdf_logo} className="pdf-logo" />
@@ -54,7 +71,11 @@ const Spdrm = () => {
           </div>
 
           <div className="innerdivs" id="pdfFilesMarginLeft">
-            <a href={SPDRM_BROCHER} download="spderm_brochers">
+            <a
+              // href={SPDRM_BROCHER}
+              onClick={() => handleDownload(SPDRM_BROCHER)}
+              download="spderm_brochers"
+            >
               <span className="colorAndFont">
                 SPDRM brochure{" "}
                 <img src={brochers_logo} className="doc-logo" alt="" />
@@ -64,7 +85,8 @@ const Spdrm = () => {
 
           <div className="innerdivs" id="pdfFilesMarginLeft">
             <a
-              href={SPDRM_SYSTEM_REQUIREMENT}
+              // href={SPDRM_SYSTEM_REQUIREMENT}
+              onClick={() => handleDownload(SPDRM_SYSTEM_REQUIREMENT)}
               download="spdrm_system_requirements"
             >
               <span className="colorAndFont">
@@ -75,7 +97,11 @@ const Spdrm = () => {
           </div>
 
           <div className="innerdivs" id="pdfFilesMarginLeft">
-            <a href={SPDRM_QA} download="spdrm_qa">
+            <a
+              // href={SPDRM_QA}
+              onClick={() => handleDownload(SPDRM_QA)}
+              download="spdrm_qa"
+            >
               <span className="colorAndFont">
                 SPDRM Q&A
                 <img src={pdf_logo} className="pdf-logo" />
@@ -84,7 +110,11 @@ const Spdrm = () => {
           </div>
 
           <div className="innerdivs" id="pdfFilesMarginLeft">
-            <a href={SPDRM_SUPERIORITY} download="spdrm_superiority">
+            <a
+              // href={SPDRM_SUPERIORITY}
+              onClick={() => handleDownload(SPDRM_SUPERIORITY)}
+              download="spdrm_superiority"
+            >
               <span className="colorAndFont">
                 SPDRM superiority
                 <img src={pdf_logo} className="pdf-logo" />
@@ -94,7 +124,8 @@ const Spdrm = () => {
 
           <div className="innerdivs" id="pdfFilesMarginLeft">
             <a
-              href={SPDRM_VEHICLE_COLLISION}
+              // href={SPDRM_VEHICLE_COLLISION}
+              onClick={() => handleDownload(SPDRM_VEHICLE_COLLISION)}
               download="Vehicle_collision_simulation_process_management_using_SPDRM"
             >
               <span className="colorAndFont">
@@ -106,7 +137,8 @@ const Spdrm = () => {
 
           <div className="innerdivs" id="pdfFilesMarginLeft">
             <a
-              href={EXE_DESIGN_PROCESS}
+              // href={EXE_DESIGN_PROCESS}
+              onClick={() => handleDownload(EXE_DESIGN_PROCESS)}
               download="Execution_and_Design_Processes.pdf"
             >
               <span className="colorAndFont">
@@ -118,7 +150,8 @@ const Spdrm = () => {
 
           <div className="innerdivs" id="pdfFilesMarginLeft">
             <a
-              href={POWER_OF_PROCESS_MANAGMENT}
+              // href={POWER_OF_PROCESS_MANAGMENT}
+              onClick={() => handleDownload(POWER_OF_PROCESS_MANAGMENT)}
               download="Unlocking_the_power_of_process_management_with_SPDRM"
             >
               <span className="colorAndFont">
@@ -136,7 +169,11 @@ const Spdrm = () => {
           </div>
 
           <div className="innerdivs" id="pdfFilesMarginLeft">
-            <a href={MULTI_SITE_COLLABRATION} download="multisite_collabration">
+            <a
+              // href={MULTI_SITE_COLLABRATION}
+              onClick={() => handleDownload(MULTI_SITE_COLLABRATION)}
+              download="multisite_collabration"
+            >
               <span className="colorAndFont">
                 Multi-site collaboration to unite global teams
                 <img src={pdf_logo} className="pdf-logo" />
@@ -169,7 +206,11 @@ const Spdrm = () => {
         <div className="guides">
           <span style={{ fontWeight: "700", color: "brown" }}>Guides</span>
           <div className="innerdivs" id="pdfFilesMarginLeft">
-            <a href={SPDRM_ADM_GUIDE} download="spderm_administartor_guide">
+            <a
+              // href={SPDRM_ADM_GUIDE}
+              onClick={() => handleDownload(SPDRM_ADM_GUIDE)}
+              download="spderm_administartor_guide"
+            >
               <span className="colorAndFont">
                 SPDRM administrators guide
                 <img src={pdf_logo} className="pdf-logo" />
@@ -179,7 +220,8 @@ const Spdrm = () => {
 
           <div className="innerdivs" id="pdfFilesMarginLeft">
             <a
-              href={SPDRM_INSTALLATION_GUIDE}
+              // href={SPDRM_INSTALLATION_GUIDE}
+              onClick={() => handleDownload(SPDRM_INSTALLATION_GUIDE)}
               download="spdrm_installation_guide"
             >
               <span className="colorAndFont">
@@ -190,7 +232,11 @@ const Spdrm = () => {
           </div>
 
           <div className="innerdivs" id="pdfFilesMarginLeft">
-            <a href={SPDRM_USER_GUIDE} download="spdrm_users_guide">
+            <a
+              // href={SPDRM_USER_GUIDE}
+              onClick={() => handleDownload(SPDRM_USER_GUIDE)}
+              download="spdrm_users_guide"
+            >
               <span className="colorAndFont">
                 SPDRM user guide <img src={pdf_logo} className="pdf-logo" />
               </span>
